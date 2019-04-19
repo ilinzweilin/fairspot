c2e426e35adb6a76f8db31015f9f972704e23b83
ilinzweilin <sinelnikovaaa@gmail.com>
2019-04-20 01:34:50 +0200

    add raiden communication

--- src/payment.js
+++ src/payment.js
@@ -1,11 +1,43 @@
-async function openPaymentChannel () {}
+let axios = require('axios')
 
-async function closePaymentChannel () {
-  bytesLeft = 0
+async function openPaymentChannel(partnerAddress, tokenAddress, totalDeposit, settleTimeout) {
+  const req = axios.put(
+    'http://localhost:5001/api/v1/channels',
+    {
+      partner_address: partnerAddress,
+      token_address: tokenAddress,
+      total_deposit: totalDeposit,
+      settle_timeout: settleTimeout
+    },
+    {
+      headers: { 'Content-Type': 'application/json' },
+    }
+  )
+  console.log("payment channel opened", req)
 }
 
-async function submitPayment () {
-  console.log('should pay $$$$$')
+async function closePaymentChannel(partnerAddress, tokenAddress) {
+  const req = axios.patch(
+    `http://localhost:5001/api/v1/channels/${partnerAddress}/${tokenAddress}`,
+    {},
+    {
+      headers: { 'Content-Type': 'application/json' },
+    }
+  )
+  console.log("payment channel closed", req)
+}
+
+async function submitPayment(partnerAddress, tokenAddress, amount, identifier) {
+  const req = axios.post(
+    `http://localhost:5001/api/v1/channels/${partnerAddress}/${tokenAddress}`,
+    {
+      amount,
+      identifier
+    },
+    {
+      headers: { 'Content-Type': 'application/json' },
+    })
+  console.log("payment sent", req)
 }
 
 module.exports = {
